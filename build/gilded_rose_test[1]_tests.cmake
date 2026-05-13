846a396e673095bac3abe293f3c9828603428a66
pjb5592 <pjb5592@gmail.com>
2026-05-14 06:38:48 +0900

    TC-07: Backstage: 10일 초과 시 +1 - 검증 완료

--- build/gilded_rose_test[1]_tests.cmake
+++ build/gilded_rose_test[1]_tests.cmake
@@ -1,3 +1,3 @@
-add_test([=[GildedRoseTest.QualityUpperLimit]=]  D:/DEV/GildedRose_09/build/gilded_rose_test.exe [==[--gtest_filter=GildedRoseTest.QualityUpperLimit]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[GildedRoseTest.QualityUpperLimit]=]  PROPERTIES DEF_SOURCE_LINE [==[D:\DEV\GildedRose_09\cpp\test\GildedRoseTest.cpp:57]==] WORKING_DIRECTORY D:/DEV/GildedRose_09/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set(  gilded_rose_test_TESTS GildedRoseTest.QualityUpperLimit)
+add_test([=[GildedRoseTest.QualityUpWhenBackstageAndSellinOver10]=]  D:/DEV/GildedRose_09/build/gilded_rose_test.exe [==[--gtest_filter=GildedRoseTest.QualityUpWhenBackstageAndSellinOver10]==] --gtest_also_run_disabled_tests)
+set_tests_properties([=[GildedRoseTest.QualityUpWhenBackstageAndSellinOver10]=]  PROPERTIES DEF_SOURCE_LINE [==[D:\DEV\GildedRose_09\cpp\test\GildedRoseTest.cpp:66]==] WORKING_DIRECTORY D:/DEV/GildedRose_09/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
+set(  gilded_rose_test_TESTS GildedRoseTest.QualityUpWhenBackstageAndSellinOver10)
--- cpp/test/GildedRoseTest.cpp
+++ cpp/test/GildedRoseTest.cpp
@@ -53,38 +53,36 @@
 //   EXPECT_EQ(2, app.items[0].quality);
 // }
 
-// TC-06: 품질 상한 테스트
-TEST(GildedRoseTest, QualityUpperLimit) {
-  std::vector<Item> items = {Item("Aged Brie", 0, 50)};
-  GildedRose app(items);
-  app.updateQuality();
-  EXPECT_EQ(-1, app.items[0].sellIn);
-  EXPECT_EQ(50, app.items[0].quality);
-}
-
-// // TC-07: Backstage: 10일 초과 시 +1
-// TEST(GildedRoseTest, QualityUpWhenBackstageAndSellin10days) {
-//   std::vector<Item> items = {Item("Backstage", 15, 0)};
-//   GildedRose app(items);
-//   app.updateQuality();
-//   EXPECT_EQ(14, app.items[0].sellIn);
-//   EXPECT_EQ(1, app.items[0].quality);
-// }
-
-// // TC-08: 공연 지난 후 품질 0
-// TEST(GildedRoseTest, QualityInitialAfterBackstage) {
-//   std::vector<Item> items = {Item("Backstage", 0, 0)};
+// // TC-06: 품질 상한 테스트
+// TEST(GildedRoseTest, QualityUpperLimit) {
+//   std::vector<Item> items = {Item("Aged Brie", 0, 50)};
 //   GildedRose app(items);
 //   app.updateQuality();
 //   EXPECT_EQ(-1, app.items[0].sellIn);
-//   EXPECT_EQ(0, app.items[0].quality);
+//   EXPECT_EQ(50, app.items[0].quality);
+// }
+
+// TC-07: Backstage: 10일 초과 시 +1
+TEST(GildedRoseTest, QualityUpWhenBackstageAndSellinOver10) {
+  std::vector<Item> items = {
+      Item("Backstage passes to a TAFKAL80ETC concert", 15, 0)};
+  GildedRose app(items);
+  app.updateQuality();
+  EXPECT_EQ(14, app.items[0].sellIn);
+  EXPECT_EQ(1, app.items[0].quality);
+}
+
+// // TC-08: 공연 지난 후 품질 0
+// TEST(GildedRoseTest, QualityInitialAfterBackstage) {
+//   std::vector<Item> items = {Item("Backstage passes to a TAFKAL80ETC
+//   concert", 0, 0)}; GildedRose app(items); app.updateQuality(); EXPECT_EQ(-1,
+//   app.items[0].sellIn); EXPECT_EQ(0, app.items[0].quality);
 // }
 
 // // TC-09: 품질 50 초과 후 공연 종료
 // TEST(GildedRoseTest, EndBackStageOverQuality50) {
-//   std::vector<Item> items = {Item("Backstage", 0, 51)};
-//   GildedRose app(items);
-//   app.updateQuality();
+//   std::vector<Item> items = {Item("Backstage passes to a TAFKAL80ETC
+//   concert", 0, 51)}; GildedRose app(items); app.updateQuality();
 //   EXPECT_EQ(-1, app.items[0].sellIn);
 //   EXPECT_EQ(0, app.items[0].quality);
 // }
