970750c721385ee11fdea486920c7fa06bb180c8
pjb5592 <pjb5592@gmail.com>
2026-05-13 16:15:42 +0900

    README.md 파일 수정

--- cpp/test/GildedRoseTest.cpp
+++ cpp/test/GildedRoseTest.cpp
@@ -1,9 +1,17 @@
-#include <gtest/gtest.h>
 #include "GildedRose.h"
+#include <gtest/gtest.h>
 
 TEST(GildedRoseTest, Foo) {
-    std::vector<Item> items = { Item("foo", 0, 0) };
-    GildedRose app(items);
-    app.updateQuality();
-    EXPECT_EQ("fixme", app.items[0].name);
+  std::vector<Item> items = {Item("foo", 0, 0)};
+  GildedRose app(items);
+  app.updateQuality();
+  EXPECT_EQ("fixme", app.items[0].name);
+}
+
+//
+TEST(GildedRoseTest, TC_01) {
+  std::vector<Item> items = {Item("foo", 0, 0)};
+  GildedRose app(items);
+  app.updateQuality();
+  EXPECT_EQ("fixme", app.items[0].name);
 }
