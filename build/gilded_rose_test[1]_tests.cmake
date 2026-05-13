f3ef5b505b88378dfd40f16220dc233e0d871091
pjb5592 <pjb5592@gmail.com>
2026-05-13 16:22:53 +0900

    Foo TestCase 검증 - 기대값: fixme → foo

--- build/gilded_rose_test[1]_tests.cmake
+++ build/gilded_rose_test[1]_tests.cmake
@@ -1,3 +1,5 @@
 add_test([=[GildedRoseTest.Foo]=]  D:/DEV/GildedRose_09/build/gilded_rose_test.exe [==[--gtest_filter=GildedRoseTest.Foo]==] --gtest_also_run_disabled_tests)
 set_tests_properties([=[GildedRoseTest.Foo]=]  PROPERTIES DEF_SOURCE_LINE [==[D:\DEV\GildedRose_09\cpp\test\GildedRoseTest.cpp:4]==] WORKING_DIRECTORY D:/DEV/GildedRose_09/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set(  gilded_rose_test_TESTS GildedRoseTest.Foo)
+add_test([=[GildedRoseTest.CheckNormalItemQualityLowerLimit]=]  D:/DEV/GildedRose_09/build/gilded_rose_test.exe [==[--gtest_filter=GildedRoseTest.CheckNormalItemQualityLowerLimit]==] --gtest_also_run_disabled_tests)
+set_tests_properties([=[GildedRoseTest.CheckNormalItemQualityLowerLimit]=]  PROPERTIES DEF_SOURCE_LINE [==[D:\DEV\GildedRose_09\cpp\test\GildedRoseTest.cpp:12]==] WORKING_DIRECTORY D:/DEV/GildedRose_09/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
+set(  gilded_rose_test_TESTS GildedRoseTest.Foo GildedRoseTest.CheckNormalItemQualityLowerLimit)
--- cpp/test/GildedRoseTest.cpp
+++ cpp/test/GildedRoseTest.cpp
@@ -5,12 +5,12 @@ TEST(GildedRoseTest, Foo) {
   std::vector<Item> items = {Item("foo", 0, 0)};
   GildedRose app(items);
   app.updateQuality();
-  EXPECT_EQ("fixme", app.items[0].name);
+  EXPECT_EQ("foo", app.items[0].name);
 }
 
-//
-TEST(GildedRoseTest, TC_01) {
-  std::vector<Item> items = {Item("foo", 0, 0)};
+// 일반 아이템 품질 하한
+TEST(GildedRoseTest, CheckNormalItemQualityLowerLimit) {
+  std::vector<Item> items = {Item("noname", 0, 0)};
   GildedRose app(items);
   app.updateQuality();
   EXPECT_EQ("fixme", app.items[0].name);
