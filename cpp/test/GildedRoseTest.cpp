5ab57f17b0c3b11e6f836d388c652d78a36a6415
pjb5592 <pjb5592@gmail.com>
2026-05-14 23:59:18 +0900

    Golden Mater 파일 생성 - F&B

--- cpp/test/GildedRoseTest.cpp
+++ cpp/test/GildedRoseTest.cpp
@@ -111,8 +111,10 @@ TEST(FoodBeverageTest, DegradesTwiceAsNormal) {
   std::vector<Item> items = {Item("[F&B] Bread", 5, 20)};
   GildedRose app(items);
   app.updateQuality();
-  EXPECT_EQ(4, items[0].sellIn);
-  EXPECT_EQ(18, items[0].quality); // 20 -2 = 18
+  // EXPECT_EQ(4, items[0].sellIn);
+  // EXPECT_EQ(18, items[0].quality); // 20 -2 = 18
+
+  ApprovalTests::Approvals::verify(app.items[0].toString());
 }
 
 // TC-12: F&B 기한 지났을 때 동작
@@ -120,7 +122,9 @@ TEST(FoodBeverageTest, DegradesFourTimesAfterSellIn) {
   std::vector<Item> items = {Item("[F&B] Milk", 0, 20)};
   GildedRose app(items);
   app.updateQuality();
-  EXPECT_EQ(16, items[0].quality); // 20 -4 = 16
+  // EXPECT_EQ(16, items[0].quality); // 20 -4 = 16
+
+  ApprovalTests::Approvals::verify(app.items[0].toString());
 }
 
 // TC-13: F&B Quality 품질 하한 확인
@@ -128,5 +132,7 @@ TEST(FoodBeverageTest, QualityNeverBelowZero) {
   std::vector<Item> items = {Item("[F&B] Water", 0, 1)};
   GildedRose app(items);
   app.updateQuality();
-  EXPECT_EQ(0, items[0].quality); // 0 유지
+  // EXPECT_EQ(0, items[0].quality); // 0 유지
+
+  ApprovalTests::Approvals::verify(app.items[0].toString());
 }
