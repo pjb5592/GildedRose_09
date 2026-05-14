49806c31054ecfc0a28014f2a7d847ee5793c292
pjb5592 <pjb5592@gmail.com>
2026-05-14 16:23:43 +0900

    items[i]. → item.

--- cpp/src/GildedRose.cpp
+++ cpp/src/GildedRose.cpp
@@ -4,56 +4,57 @@ GildedRose::GildedRose(std::vector<Item> &items) : items(items) {}
 
 void GildedRose::updateQuality() {
   for (size_t i = 0; i < items.size(); i++) {
+    Item &item = items[i];
 
-    if (items[i].name.substr(0, 8) == "Sulfuras")
+    if (item.name.substr(0, 8) == "Sulfuras")
       continue;
 
-    if (items[i].name != AGED_BRIE && items[i].name != BACKSTAGE_PASS) {
-      if (items[i].quality > MIN_QUALITY) {
-        if (items[i].name != SULFURAS) {
-          items[i].quality = items[i].quality - 1;
+    if (item.name != AGED_BRIE && item.name != BACKSTAGE_PASS) {
+      if (item.quality > MIN_QUALITY) {
+        if (item.name != SULFURAS) {
+          item.quality = item.quality - 1;
         }
       }
     } else {
-      if (items[i].quality < MAX_QUALITY) {
-        items[i].quality = items[i].quality + 1;
+      if (item.quality < MAX_QUALITY) {
+        item.quality = item.quality + 1;
 
-        if (items[i].name == BACKSTAGE_PASS) {
-          if (items[i].sellIn < 11) {
-            if (items[i].quality < MAX_QUALITY) {
-              items[i].quality = items[i].quality + 1;
+        if (item.name == BACKSTAGE_PASS) {
+          if (item.sellIn < 11) {
+            if (item.quality < MAX_QUALITY) {
+              item.quality = item.quality + 1;
             } else {
-              items[i].sellIn = -1; //
+              item.sellIn = -1; //
             }
           }
 
-          if (items[i].sellIn < 6) {
-            if (items[i].quality < MAX_QUALITY) {
-              items[i].quality = items[i].quality + 1;
+          if (item.sellIn < 6) {
+            if (item.quality < MAX_QUALITY) {
+              item.quality = item.quality + 1;
             }
           }
         }
       }
     }
 
-    if (items[i].name != SULFURAS) {
-      items[i].sellIn = items[i].sellIn - 1;
+    if (item.name != SULFURAS) {
+      item.sellIn = item.sellIn - 1;
     }
 
-    if (items[i].sellIn < 0) {
-      if (items[i].name != AGED_BRIE) {
-        if (items[i].name != BACKSTAGE_PASS) {
-          if (items[i].quality > MIN_QUALITY) {
-            if (items[i].name != SULFURAS) {
-              items[i].quality = items[i].quality - 1;
+    if (item.sellIn < 0) {
+      if (item.name != AGED_BRIE) {
+        if (item.name != BACKSTAGE_PASS) {
+          if (item.quality > MIN_QUALITY) {
+            if (item.name != SULFURAS) {
+              item.quality = item.quality - 1;
             }
           }
         } else {
-          items[i].quality = items[i].quality - items[i].quality;
+          item.quality = item.quality - item.quality;
         }
       } else {
-        if (items[i].quality < MAX_QUALITY) {
-          items[i].quality = items[i].quality + 1;
+        if (item.quality < MAX_QUALITY) {
+          item.quality = item.quality + 1;
         }
       }
     }
