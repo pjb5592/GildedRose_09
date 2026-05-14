278987885ec61bd14bcbaf5a182a316bd18015bc
pjb5592 <pjb5592@gmail.com>
2026-05-14 23:45:52 +0900

    변수 분리

--- cpp/src/GildedRose.cpp
+++ cpp/src/GildedRose.cpp
@@ -11,17 +11,20 @@
 GildedRose::GildedRose(std::vector<Item> &items) : items(items) {}
 
 std::unique_ptr<GildedRoseItem> GildedRose::createItem(Item &item) {
+  std::unique_ptr<GildedRoseItem> gi;
+
   if (item.name == SULFURAS) {
-    return std::make_unique<SulfurasItem>(item);
+    gi = std::make_unique<SulfurasItem>(item);
   } else if (item.name == AGED_BRIE) {
-    return std::make_unique<AgedBrieItem>(item);
+    gi = std::make_unique<AgedBrieItem>(item);
   } else if (item.name == BACKSTAGE_PASS) {
-    return std::make_unique<BackStagePassItem>(item);
+    gi = std::make_unique<BackStagePassItem>(item);
   } else if (item.name.substr(0, 5) == FOODANDBEVERAGE) {
-    return std::make_unique<FoodBeverageItem>(item);
+    gi = std::make_unique<FoodBeverageItem>(item);
   } else {
-    return std::make_unique<NormalItem>(item);
+    gi = std::make_unique<NormalItem>(item);
   }
+  return gi;
 }
 
 void GildedRose::updateQuality() {
